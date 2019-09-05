ac218388a5ab7d53b09427396d34f3223922fda0
Michael van Appeldorn <michael.vanappeldorn@cognotekt.com>
2019-09-05 14:02:06 +0200

    add no-sync msg

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = false ]] && aws s3 sync public/ s3://${BUCKET}/${BUCKET_KEY} || echo "No sync happened"
+[[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = false ]] && aws s3 sync public/ s3://${BUCKET}/${BUCKET_KEY} || echo "No sync done. Only sync when Pull Request -> Master"
