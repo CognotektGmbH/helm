8e4e5a963374c456b68e095271cc4f2afa8e33cd
Michael van Appeldorn <michael.vanappeldorn@cognotekt.com>
2019-09-05 13:28:28 +0200

    add sync pt2

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = false ]] && aws s3 sync public/ s3://${BUCKET}/${BUCKET_KEY}
-[[ ! -z  "$TRAVIS_BRANCH" ]] && echo "TRAVIS_BRANCH not empty: ---$TRAVIS_BRANCH---"
-[[ ! -z  "$TRAVIS_PULL_REQUEST" ]] && echo "TRAVIS_PULL_REQUEST not empty: ---$TRAVIS_PULL_REQUEST---"
+#[[ ! -z  "$TRAVIS_BRANCH" ]] && echo "TRAVIS_BRANCH not empty: ---$TRAVIS_BRANCH---"
+#[[ ! -z  "$TRAVIS_PULL_REQUEST" ]] && echo "TRAVIS_PULL_REQUEST not empty: ---$TRAVIS_PULL_REQUEST---"
