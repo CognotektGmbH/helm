012b4bc8007c1e5891ae5f8345d4acf63fa5e659
Michael van Appeldorn <michael.vanappeldorn@cognotekt.com>
2019-09-05 13:36:34 +0200

    add else condition

--- sync.sh
+++ sync.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = false ]] && aws s3 sync public/ s3://${BUCKET}/${BUCKET_KEY} || echo "No sync happened"
-#[[ ! -z  "$TRAVIS_BRANCH" ]] && echo "TRAVIS_BRANCH not empty: ---$TRAVIS_BRANCH---"
-#[[ ! -z  "$TRAVIS_PULL_REQUEST" ]] && echo "TRAVIS_PULL_REQUEST not empty: ---$TRAVIS_PULL_REQUEST---"
